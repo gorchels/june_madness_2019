08af2b35100309ae72dbaaed5e8837484a9eb71a
gorchels <gorchels@bren.ucsb.edu>
2019-06-11 12:04:32 -0700

    Made it so the months went in chronlogical order

--- R/SeasonHumd.R
+++ R/SeasonHumd.R
@@ -6,7 +6,7 @@
 #'
 #'
 #'@param city is your input table. See the package description and example data to see the format required
-#'
+#'This function will return a tibble with the average humidity for each of the months of your baseball season
 SeasonHumd = function(city) {
   average_humd = city %>%
     group_by(month) %>%
--- R/june.R
+++ R/june.R
@@ -5,10 +5,11 @@
 #'June
 #'
 #'@param city is your input table. See the package description and example data to see the format required
-#'
+#''This function will return a tibble with the average humidity for each of the months of your baseball season
 june = function(city) {
   group_by(city, month)
   average_temp = city %>%
-    summarise(avg_temp = mean(high))
+    summarise(avg_temp = mean(high))%>%
+    arrange(match(month, month.name))
 }
 
