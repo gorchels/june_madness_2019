a97f34ecc53ff544405242b23192facdb3703867
gorchels <gorchels@bren.ucsb.edu>
2019-06-11 12:57:19 -0700

    Fixed bug in the decription and started the homerun calculation

---
+++ R/homeruns.R
@@ -0,0 +1,16 @@
+
+#
+#
+#
+#'Humidity Summary
+#'
+#'
+#'@param average_humd is the output from the SeasonHumd function
+#'This assumes that humidity below 50% will not have a significant effect on homeruns
+homeruns = function(ah) {
+  homerun_inc = ah %>%
+    (mutate(prob_increase = case_when((avg_humd - 50)>5 ~"1%",
+                               (avg_humd - 50)>10 ~"2%",
+                               (avg_humd - 50)<=0 ~"0%")))
+  return(homerun_inc)
+}
