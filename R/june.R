96a8cdcf5fde5f35416342fb205cea931e7171b0
gorchels <gorchels@bren.ucsb.edu>
2019-06-11 12:15:06 -0700

    Fixed bug in June function

--- R/june.R
+++ R/june.R
@@ -7,9 +7,10 @@
 #'@param city is your input table. See the package description and example data to see the format required
 #''This function will return a tibble with the average humidity for each of the months of your baseball season
 june = function(city) {
-  group_by(city, month)
-  average_temp = city %>%
-    summarise(avg_temp = mean(high))%>%
+  average_high = city %>%
+    group_by(month) %>%
+    summarise(avg_high = mean(high))%>%
     arrange(match(month, month.name))
+  return(average_high)
 }
 
