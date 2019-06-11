ac2ae686eb12b1fde7ca1d8a0eaae4c78cc4efd9
gorchels <gorchels@bren.ucsb.edu>
2019-06-11 11:52:48 -0700

    Formatting

--- R/SeasonHumd.R
+++ R/SeasonHumd.R
@@ -4,7 +4,7 @@
 #
 #'Humidity Summary
 #'
-#'Is the best madness
+#'
 #'@param city is your input table. See the package description and example data to see the format required
 #'
 SeasonHumd = function(city) {
