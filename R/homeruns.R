69549720778dcdb877a042be1c5935a5fe39958c
gorchels <gorchels@bren.ucsb.edu>
2019-06-12 09:32:24 -0700

    We have a functional homerun increase function

--- R/homeruns.R
+++ R/homeruns.R
@@ -8,8 +8,9 @@
 #'@param ah is the output from the SeasonHumd function
 #'This assumes that humidity below 50% will not have a significant effect on homeruns
 homeruns = function(ah) {
-  homerun_inc = mutate(ah, prob_increase = case_when((avg_humd - 50)>5 ~"1%",
-                               (avg_humd - 50)>10 ~"2%",
-                               (avg_humd - 50)<=5 ~"0%"))
+  homerun_inc = mutate(ah, prob_increase = case_when(
+    (avg_humd - 50)>10 ~"2%",
+    (avg_humd - 50)>5 ~"1%",
+    (avg_humd - 50)<=5 ~"0%"))
   return(homerun_inc)
 }
