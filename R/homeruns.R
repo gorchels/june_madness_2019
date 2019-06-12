73d52f9182d8a385b581532581f11f63ac928c79
gorchels <gorchels@bren.ucsb.edu>
2019-06-12 09:35:38 -0700

    Added % increase options

--- R/homeruns.R
+++ R/homeruns.R
@@ -9,6 +9,13 @@
 #'This assumes that humidity below 50% will not have a significant effect on homeruns
 homeruns = function(ah) {
   homerun_inc = mutate(ah, prob_increase = case_when(
+    (avg_humd - 50)>45 ~"9%",
+    (avg_humd - 50)>40 ~"8%",
+    (avg_humd - 50)>35 ~"7%",
+    (avg_humd - 50)>30 ~"6%",
+    (avg_humd - 50)>25 ~"5%",
+    (avg_humd - 50)>20 ~"4%",
+    (avg_humd - 50)>15 ~"3%",
     (avg_humd - 50)>10 ~"2%",
     (avg_humd - 50)>5 ~"1%",
     (avg_humd - 50)<=5 ~"0%"))
