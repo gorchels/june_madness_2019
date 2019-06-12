f7da8605bb214434f59ef6de45ccd10ce17e1a79
gorchels <gorchels@bren.ucsb.edu>
2019-06-12 09:49:28 -0700

    homerun documentation

--- R/homeruns.R
+++ R/homeruns.R
@@ -2,11 +2,11 @@
 #
 #
 #
-#'homeruns summary
+#'Homerun Probability Increases From Humidity
 #'
-#'
+#'This function converts the average humidity to increases in homerun probabilities.
 #'@param ah is the output from the SeasonHumd function
-#'This assumes that humidity below 50% will not have a significant effect on homeruns
+#'This assumes that humidity below 50 percent will not have a significant effect on homeruns
 homeruns = function(ah) {
   homerun_inc = mutate(ah, prob_increase = case_when(
     (avg_humd - 50)>45 ~"9%",
