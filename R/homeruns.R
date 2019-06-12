4533a698e3ce8ec6c4e7096e03c6f67bdc0e63ea
gorchels <gorchels@bren.ucsb.edu>
2019-06-12 08:55:32 -0700

    I've got the homerun package working!

--- R/homeruns.R
+++ R/homeruns.R
@@ -8,9 +8,8 @@
 #'@param ah is the output from the SeasonHumd function
 #'This assumes that humidity below 50% will not have a significant effect on homeruns
 homeruns = function(ah) {
-  homerun_inc = ah %>%
-    (mutate(prob_increase = case_when((avg_humd - 50)>5 ~"1%",
+  homerun_inc = (mutate(ah, prob_increase = case_when((avg_humd - 50)<=0 ~"0%"
                                (avg_humd - 50)>10 ~"2%",
-                               (avg_humd - 50)<=0 ~"0%")))
+                               (avg_humd - 50)>5 ~"1%")))
   return(homerun_inc)
 }
