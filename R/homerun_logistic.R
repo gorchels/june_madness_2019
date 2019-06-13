717a07256a05378e22703d82f35e98f0557b87cc
kirbybartlett <kirbybartlett47@gmail.com>
2019-06-12 21:56:37 -0700

    updated home run model

--- R/homerun_logistic.R
+++ R/homerun_logistic.R
@@ -2,17 +2,20 @@
 #'
 #'Computes whether there is a noticable association between weather and average ticket sales revenue
 #'@param homerun Binoary response variable stating whether or not a home run was hit during that days game
-#'@param storm vector containing information on whether or not there was a thunderstorm the day of the baseball game
-#'@param temp vector containing the mean temperature for the day the baseball game was on
-#'@param humid vector containing the max humidity for the day the baseball game was on
-#'@return a summary output of the regression
+#'@param temp_arrau vector containing the mean temperature for the day the baseball game was on
+#'@param humid_array vector containing the max humidity for the day the baseball game was on
+#'@param temperature a temperature of interest to plug into the binomial logistic regression
+#'@param humidity a humidity of interest to plug into the binomial logistic regression
+#'@return The probability of there being a home run based on your inputted arrays and temperature and humidity of interest
 #'
 #'
 
 
-homerun_probability_computation <- function(homerun, storm, temp, humid) {
-  homerun_glm<-glm(homerun ~ storm + temp + humid, family = "binomial")
-  odds <- exp(coef(homerun_glm))
+homerun_probability_computation <- function(homerun_array, temp_array, humid_array, temperature, humidity) {
+  library(kableExtra)
+  logit<-glm(homerun_array ~ temp_array + humid_array, family = "binomial")
+  output <- coef(logit)[1] + coef(logit)[2]*temperature + coef(logit)[3]*humidity
+  odds <- exp(output)
   prob <- odds / (1 + odds)
-  return(prob)
+  kableExtra::kable(prob, col.names = "Probability of Home Run")
 }
