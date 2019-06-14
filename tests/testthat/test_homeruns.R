e5cec5a6902081431a30735d07475c802b225c3b
gorchels <gorchels@bren.ucsb.edu>
2019-06-13 17:07:33 -0700

    Working on the homerun tests

--- tests/testthat/test_homeruns.R
+++ tests/testthat/test_homeruns.R
@@ -5,10 +5,11 @@ test_that(
       as.data.frame(
         cbind(
           month = "March",
-          humidity   =0
+          avg_humd   =50
         )
       )
 
-    expect_that(mean(homeruns(boston_data)$avg_humd) > 0)
+    expect_that((homeruns(boston_data)$avg_humd) = "0%")
   }
 )
+
